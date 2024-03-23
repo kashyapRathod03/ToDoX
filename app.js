f1b6603e1de5e9cf7887977e1427672f9901c335
kashyap <kashyaprathod03@gmail.com>
2024-03-23 11:49:47 +0530

    build folder added

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const app = express();
 require("./db/conn");
 const cors = require("cors");
 const router = require("./Routes/router");
-const PORT = process.env.PORT || 6010
+const PORT = process.env.PORT || 6010;
+const fs = require('fs');
+const path = require('path');
 
 
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:3000","https://conversa-backend-tymi.onrender.com"],
+    credentials: true,
+    // origin: process.env.CLIENT_URL,
+  }));
 app.use(express.json());
 app.use("/uploads",express.static("./uploads"));
 app.use("/files",express.static("./public/files"));
 
 app.use(router);
+
+app.use(express.static(path.join(__dirname,"./frontend/build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./frontend/build/index.html"))
+});
+
 
 app.listen(PORT,()=>{
     console.log(`Server start at port no ${PORT}`)
